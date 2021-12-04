25033a53b1c0a6f658b18cd4d36d39193106bac1
Nishino0719 <nishino0719@icloud.com>
2021-12-04 21:38:30 +0900

    🚀 deploy contracts in ropsten network

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,24 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
 
-describe("Greeter", function () {
+describe('Greeter', function () {
+  this.timeout(60 * 1000)
   it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+    const Greeter = await ethers.getContractFactory('Greeter')
+    const greeter = await Greeter.deploy('Hello, world!')
+    await greeter.deployed()
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    console.log(
+      `Greeter Deploy Tx: https://ropsten.etherscan.io/tx/${greeter.deployTransaction.hash}`
+    )
+    console.log(`Greeter Contract Address: ${greeter.address}`)
+    expect(await greeter.greet()).to.equal('Hello, world!')
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const setGreetingTx = await greeter.setGreeting('Hola, mundo!')
 
     // wait until the transaction is mined
-    await setGreetingTx.wait();
+    await setGreetingTx.wait()
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+    expect(await greeter.greet()).to.equal('Hola, mundo!')
+  })
+})
