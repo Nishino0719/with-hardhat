e0c9e83efa4105ba5e5ff014611352e7ce0878db
Nishino0719 <nishino0719@icloud.com>
2021-12-12 10:30:02 +0900

    🔨

--- src/pages/greet.tsx
+++ src/pages/greet.tsx
@@ -1,19 +1,20 @@
 import React, { useEffect } from 'react'
 import Link from 'next/link'
+import { DAppProviderLayout } from '../components/DAppProviderLayout'
+import { UseDAppTestComponent } from '../components/UseDAppTestComponent'
 
 export default function Greet() {
-  useEffect(() => {}, [])
-
   return (
-    <div className="mb-20 text-center">
+    <DAppProviderLayout>
       <p className="mt-10 text-4xl font-bold">
         This is trying out page of SmartContract
       </p>
+      <UseDAppTestComponent />
       <Link href="/">
         <button className="w-64 p-2 my-5 font-bold duration-200 transform border-2 rounded-full shadow-md animate-pulse hover:scale-110">
           Go back to home
         </button>
       </Link>
-    </div>
+    </DAppProviderLayout>
   )
 }
